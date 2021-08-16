45ff8521659583ebaf02187a70bf9d709cfad4f0
patron <ogi.fordevelopment@gmail.com>
2021-08-16 23:46:58 +0900

    update

--- AHC004/ahc004_v3.py
+++ AHC004/ahc004_v3.py
@@ -2,7 +2,7 @@ import sys
 input=sys.stdin.readline
 import random
 import time
-DEBUG=False
+DEBUG=True
 al=list("ABCDEFGH")
 time0=time.time()
 if DEBUG:
@@ -29,7 +29,24 @@ if DEBUG:
 else:
     N,M=map(int,input().split())
     s=[input().rstrip() for i in range(M)]
-
+s_init=s[:] # scoring用
+def erase(st):
+    skip=[0]*len(s)
+    for i in range(len(s)):
+        for j in range(len(s)):
+            if i!=j and s[i] in s[j]:
+                skip[i]=1
+                break
+            elif i!=j and len(s[j])==20 and s[i] in s[j]*2:
+                skip[i]=1
+                break
+    new_s=[]
+    for i in range(len(s)):
+        if not skip[i]:
+            new_s.append(s[i])
+    return new_s
+s=erase(s)
+M=len(s)
 candi=[]
 b=[[0]*M for i in range(M)]
 for i in range(M):
@@ -49,6 +66,7 @@ for i in range(M):
 candi.sort(reverse=True)
 flg=[0]*M # used or not
 idx=0
+res=[]
 for _ in range(N):
     while True:
         l,i,j=candi[idx]
@@ -71,4 +89,46 @@ for _ in range(N):
             flg[k]=1
             break
         if len(ans)>N:break
-    print(ans[:N])
+    res.append(ans[:N])
+# 局所探索+scoring
+def calc_score(arr):
+    candi=[]
+    for i in range(N):
+        candi.append("".join(arr[i])*2)
+    for i in range(N):
+        tmp=[arr[j][i] for j in range(N)]
+        candi.append("".join(tmp)*2)
+    c=0
+    for ss in s_init:
+        for xx in candi:
+            if ss in xx:
+                c+=1
+                break
+    return round(c*10**8/M)
+max_sc=calc_score(res)
+cnt_loop=0
+print(max_sc)
+while time.time()-time0<2.75:
+    cnt_loop+=1
+    if cnt_loop%2:
+        new_res=[[res[i][j] for j in range(N)] for i in range(N)]
+        x=random.randint(0,N-1)
+        y=random.randint(1,N-1)
+        new_res[x]=new_res[x][y:]+new_res[x][:y]
+        n_sc=calc_score(new_res)
+        if n_sc>max_sc:
+            max_sc=n_sc
+            res=[[new_res[i][j] for j in range(N)] for i in range(N)]
+    else:
+        new_res=[[res[i][j] for j in range(N)] for i in range(N)]
+        x=random.randint(0,N-1)
+        y=random.randint(0,N-1)
+        new_res[x],new_res[y]=new_res[y],new_res[x]
+        n_sc=calc_score(new_res)
+        if n_sc>max_sc:
+            max_sc=n_sc
+            res=[[new_res[i][j] for j in range(N)] for i in range(N)]
+print(cnt_loop)
+print(calc_score(res))
+for i in range(N):
+    print("".join(res[i]))
